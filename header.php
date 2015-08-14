02cb933372ffc791b5d973b6f97338c09291300c
demiJade <sy_judy@yahoo.com>
2015-08-13 18:36:02 -0700

    added header.php
    you need to add these code to index.php to see the header
    <?php get_header(); ?>
    
    <link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">

---
+++ header.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<title>Elements Magazine: Ateneo Celadon's Official Publication</title>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+<body>
+<div id="wrapper">
+<div id="header">
+
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      	</button>
+		      	<a class="navbar-brand" href="http://elements.ateneo-celadon.org/">Elements</a>
+	      	</div>
+	      	<br>
+	      	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      		<ul class="nav navbar-nav navbar-right">
+	      			<li><a href="#">HOME</a></li>
+	      			<li><a href="#">SECTIONS</a></li>
+	      			<li><a href="#">ABOUT US</a></li>
+	      			<li><a href="#">CELADON</a></li>
+	      			<li><a href="#">PAST ISSUES</a></li>
+	      		</ul>
+	      	</div>
+	    </div>
+	</nav>
+</div>
