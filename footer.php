a5afe639ca4834542f71433d254cee42131e57cf
imrobberting <robert.cuartero@obf.ateneo.edu>
2015-08-25 22:21:37 +0800

    footer fix

--- footer.php
+++ footer.php
@@ -1,17 +1,19 @@
 <div id="footer">
-<h5>Elements Magazine</h1>
-<h5>Ateneo Celadon</h2>
-<h6>This website was developed by the Publications Department.</h4>
-	<span class="navbar-right">
-                    <ul class="nav navbar-nav">
-
-                        <li style=" font-size: 18px;">Follow Us: </li>
-                        <li><a class="icon" href="https://www.facebook.com/CeladonElementsMagazine"><img alt="Elements Magazine" id="icon" src="<?php echo get_bloginfo('template_url') ?>/images/facebook.png"/></a></li>
-                        <li><a class="icon" href="https://twitter.com/ElementsCeladon"><img alt="Elements Magazine" id="icon" src="<?php echo get_bloginfo('template_url') ?>/images/twitter.png"/></a></li>
-                        <li><a class="icon" href="#"><img alt="Elements Magazine" id="icon" src="<?php echo get_bloginfo('template_url') ?>/images/instagram.png"/></a></li>
-                    </ul>
-                </span>
-</div>
+	<div class="col-md-4">
+		<h5>Elements Magazine</h1>
+		<h5>Ateneo Celadon</h2>
+		<h6>This website was developed by the Publications Department.</h4>
+	</div>
+	<div class="col-md-4">
+		<span class="navbar-right">
+	    	<ul class="nav navbar-nav">
+				<li style=" font-size: 18px;">Follow Us: </li>
+	            <li><a class="icon" href="https://www.facebook.com/CeladonElementsMagazine"><img alt="Elements Magazine" id="icon" src="<?php echo get_bloginfo('template_url') ?>/images/facebook.png"/></a></li>
+	            <li><a class="icon" href="https://twitter.com/ElementsCeladon"><img alt="Elements Magazine" id="icon" src="<?php echo get_bloginfo('template_url') ?>/images/twitter.png"/></a></li>
+	            <li><a class="icon" href="#"><img alt="Elements Magazine" id="icon" src="<?php echo get_bloginfo('template_url') ?>/images/instagram.png"/></a></li>
+	         </ul>
+	    </span>
+	</div>
 </div>
 <script src="<?php echo get_bloginfo('template_url') ?>/bootstrap/js/bootstrap.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
