a55796dc2aedefd71cdfe62fd9531655001a91da
demiJade <sy_judy@yahoo.com>
2015-08-25 19:16:41 +0800

    removed redundant icons

--- single.php
+++ single.php
@@ -19,14 +19,7 @@ Template Name: Article Page
 
 <?php endwhile; ?>
 <?php endif; ?>
-<div class="navbar-right">
-					<ul class="nav navbar-nav">
-						<li style=" font-size: 18px;">Follow Us: </li>
-						<li><a class="icon" href="https://www.facebook.com/CeladonElementsMagazine"><img alt="Elements Magazine" id="icon" src="<?php echo get_bloginfo('template_url') ?>/images/facebook.png"/></a></li>
-						<li><a class="icon" href="https://twitter.com/ElementsCeladon"><img alt="Elements Magazine" id="icon" src="<?php echo get_bloginfo('template_url') ?>/images/twitter.png"/></a></li>
-						<li><a class="icon" href="#"><img alt="Elements Magazine" id="icon" src="<?php echo get_bloginfo('template_url') ?>/images/instagram.png"/></a></li>
-					</ul>
-				</div>
+
 </div>
 
 <script src="<?php echo get_bloginfo('template_url') ?>/bootstrap/js/bootstrap.min.js"></script>
