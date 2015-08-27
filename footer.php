5b81f9267cb817f151d103df092b2e151dba2d12
demiJade <sy_judy@yahoo.com>
2015-08-27 22:19:43 +0800

    fixed fotter

--- footer.php
+++ footer.php
@@ -8,18 +8,18 @@
 	</div>
 	<div class="col-md-4">
 		<span class="navbar-right">
-	    		<div>
+	    		<div class="row">
 					<div class="col-md-12" style=" font-size: 18px;">Follow Us: </div>
 				</div>
-				<div>
+				<div class="row">
 	            	<div class="col-md-2"><a class="icon" href="https://www.facebook.com/CeladonElementsMagazine"><img alt="Elements Magazine" id="icon" src="<?php echo get_bloginfo('template_url') ?>/images/facebook1.png"/></a></div>
 	            	<div class="col-md-10">Like us on Facebook!</div>
 	            </div>
-	            <div>
+	            <div class="row">
 	            	<div class="col-md-2"><a class="icon" href="https://twitter.com/ElementsCeladon"><img alt="Elements Magazine" id="icon" src="<?php echo get_bloginfo('template_url') ?>/images/Twitter_logo_white.png"/></a></div>
 	            	<div class="col-md-10">Follow Us on Twitter!</div>
 	            </div>
-	            <div>
+	            <div class="row">
 	            	<div class="col-md-2"><a class="icon" href="https://www.facebook.com/CeladonElementsMagazine"><img alt="Elements Magazine" id="icon" src="<?php echo get_bloginfo('template_url') ?>/images/facebook1.png"/></a></div>
 	            	<div class="col-md-10">Follow Us on Instagram!</div>
 	            </div>
