1a3dca9ff281408f3b4d5444deb49518c03504b0
demiJade <sy_judy@yahoo.com>
2015-08-31 18:10:54 +0800

    added favicon and changed logo

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
 <title><?php wp_title(); ?></title>
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>"> <!-- links the style.css -->
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png" />
 <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
