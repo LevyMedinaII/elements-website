a2b6b3fde25bd3b0bfae01ff8f50f4c38c4b30fc
demiJade <sy_judy@yahoo.com>
2015-09-01 22:17:21 +0800

    added about-us.php

---
+++ about-us.php
@@ -0,0 +1,28 @@
+<?php 
+/*About Us Page
+Template Name: About Us
+
+ */
+ ?>
+
+ <?php get_header(); ?>
+ <body <?php body_class( $class ); ?>>
+ 	<div class="about-us">
+ 		<img align="middle" class="team-pic" src="<?php echo get_bloginfo('template_url') ?>/images/elementsteam.jpg">
+ 		<p class="team-des">
+ 		Elements Magazine is the official publication of Ateneo de Manila University’s Chinese-Filipino organization, Ateneo Celadon. Launched in A.Y. 2014-2015, Elements Magazine, formerly known as Chinoy, aims to bridge the Chinese-Filipino culture to the greater Filipino society, beginning in the campus through its print medium, published semi-annually, and online through this website. With these, Elements hopes to embody the uniqueness of the Chinese-Filipino culture by enhancing awareness of its relevance and richness present within and beyond the campus.
+ 		</p>
+ 		<p class="team-des">
+ 			As such, dear reader, may this online release of Elements Magazine A.Y. 2015-2016 be a delight to read and a food for thought about Chinese-Filipino culture.
+ 		</p>
+ 		<p class="team-des" style="text-align:right">
+ 			On behalf of the Publications Department A.Y. 2015-2016,
+			<br>
+			<br>
+			Vice-President for Publications, Joshua Calvin Cheng
+			<br>
+			Associate Vice-President for Publications, Faith Madeleine Ong
+ 		</p>
+ 	</div>
+</body>
+ <?php get_footer(); ?>
