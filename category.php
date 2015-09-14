66c059d0f8acaa51c419198a32b0a9a5066af9e2
imrobberting <robert.cuartero@obf.ateneo.edu>
2015-09-14 16:00:36 +0800

    More changes Theme check

--- category.php
+++ category.php
@@ -67,7 +67,7 @@ $image = esc_url(get_template_directory_uri()) . '/images/placepuppy.jpg'; ?>
 </div><!-- Closes Main -->
 
 
-<script src="<?php echo get_bloginfo('template_url') ?>/bootstrap/js/bootstrap.min.js"></script>
+<script src="<?php echo esc_url(get_template_directory_uri()) ?>/bootstrap/js/bootstrap.min.js"></script>
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 
 </body>
